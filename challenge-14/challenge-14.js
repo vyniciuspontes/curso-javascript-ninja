ab39863869b263e382f4605de14af2d7698d544c
Vynicius Pontes <pontes.vynicius@gmail.com>
2018-08-25 16:03:46 -0300

    desafio 14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -1,7 +1,7 @@
 /*
 Envolva todo o código desse desafio em uma IIFE.
 */
-
+(function() {
 /*
 Crie um array chamado numberObjects. Esse array deve ter 10 elementos. Cada
 elemento será um objeto no formato:
@@ -9,23 +9,42 @@ elemento será um objeto no formato:
 Os números devem ser de 1 a 10.
 Mostre esse array no console.
 */
-console.log( 'Number Objects Array:' );
-// ?
+
+var numberObjects = [{number: 1}, {number: 2}, {number: 3}, {number: 4},
+                    {number: 5}, {number: 6}, {number: 7}, {number: 8},
+                    {number: 9}, {number: 10}]
+
+console.log( 'Number Objects Array:', numberObjects );
+
 
 /*
 Crie um array chamado `justNumbers`, que terá como elementos somente os
 números do array criado acima. Mostre esse novo array no console.
 */
-console.log( '\nJust Numbers:' );
-// ?
+
+var justNumbers = [];
+
+numberObjects.forEach(function (item) {
+    justNumbers.push(item.number);
+});
+
+console.log( '\nJust Numbers:', justNumbers );
+
 
 /*
 Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
 somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
 no console.
 */
-console.log( '\nJust module of division by 2 or 3:' );
-// ?
+
+var justMod20r3 = justNumbers.filter( function (item) {
+
+  return (item % 2 == 0 || item % 3 == 0);
+
+});
+
+console.log( '\nJust module of division by 2 or 3:', justMod20r3);
+
 
 /*
 Declare uma variável chamada operation que receba, do array criado acima,
@@ -35,16 +54,34 @@ um valor reduzido pela seguinte operação:
 O cálculo deve começar com zero.
 Mostre o resultado no console.
 */
-console.log( '\nOperation:' );
-// ?
+
+var operation = justMod20r3.reduce(function (ammount, item) {
+
+  console.log(ammount);
+
+  return (ammount + 1) * item;
+
+}, 0);
+
+console.log( '\nOperation:', operation);
+
 
 /*
 Faça o mesmo cálculo passado acima, mas começando do último item para o
 primeiro. O nome da variável deve ser operation2. Mostre o resultado no
 console.
 */
-console.log( '\nOperation 2:' );
-// ?
+
+var operation2 = justMod20r3.reduceRight(function (ammount, item) {
+
+  console.log(ammount);
+
+  return (ammount + 1) * item;
+
+}, 0);
+
+console.log( '\nOperation 2:', operation2);
+
 
 /*
 Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
@@ -54,20 +91,37 @@ PS.: Lembra da língua do "P"? Não? A língua do "P" é uma brincadeira
 infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
 falada, como se você estivesse falando em código xD
 */
-console.log( '\nSeu nome na língua do "P":' );
-// ?
+
+var name = ['Vy', 'ni', 'ci', 'us'];
+
+console.log( '\nSeu nome na língua do "P":', name.reduceRight(function(ammount, item) {
+
+  console.log('Ammount: ', ammount);
+  console.log('Item: ', item);
+
+
+  return 'p' + item + ammount;
+
+}, ''));
+
 
 /*
 Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
 e atribuirá o seu nome invertido (usando o array criado acima).
 */
-console.log( '\nInversed Name:' );
-// ?
+
+var inverseName = name.reduceRight(function (ammount, item) {
+
+    return ammount + item;
+});
+
+console.log( '\nInversed Name:', inverseName );
+
 
 /*
 Mostre no console o array `numberObjects`.
 */
-console.log( '\nNumber objects' );
+console.log( '\nNumber objects', numberObjects );
 // ?
 
 /*
@@ -79,19 +133,26 @@ Senão, mostre a frase:
 Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
-console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+
+var hasNumber = numberObjects.indexOf( numberObjects[1] );
+
+console.log( '\nExiste um { number: 2 } em numberObjects? ', hasNumber > -1 );
 
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
 será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
-console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-// ?
+var hasNumber = numberObjects.lastIndexOf( numberObjects[1], 1 );
+console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?', hasNumber > -1 );
+
+
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
-console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:', Array.isArray(numberObjects) ? numberObjects.toString() : null );
+
+
+
+})();
